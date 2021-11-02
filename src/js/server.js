d9f2a4f7596fd8a00b839f694784f2b2f109ba3e
coolchaeyoung <ggodchae@naver.com>
2021-11-02 10:29:47 +0900

    feat: Add server.js

--- src/js/server.js
+++ src/js/server.js
@@ -1,12 +1,25 @@
 require('dotenv').config();
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 
 app.use(express.static('public'));
 app.use(express.json());
 
+app.get('/news', async (req, res) => {
+  try {
+    const { data } = await axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr&category=technology&pageSize=10&apiKey=${process.env.NEWS_API_KEY}`
+    );
+    res.send(data.articles);
+  } catch (e) {
+    console.error(e.message);
+  }
+});
+
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
